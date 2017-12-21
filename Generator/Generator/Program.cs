c1e65e4e4bcd3208b8a42758eddf87a15ff18a4a
Wei Du <wei_du@viarail.ca>
2017-12-21 10:27:03 -0500

    Support multiple alias per slot

--- Generator/Generator/Program.cs
+++ Generator/Generator/Program.cs
@@ -12,22 +12,50 @@ namespace Generator
         static void Main(string[] args)
         {
             string output = "";
-            using (StreamReader sr = new StreamReader(@"C:\Git\Alexa-Custom-Slot-Generator\Station List.csv"))
+            using (StreamReader sr = new StreamReader(@"C:\Git\Alexa-Custom-Slot-Generator\Station List multi.csv"))
             {
                 string currentLine;
                 while ((currentLine = sr.ReadLine()) != null)
                 {
-                    
-                    output = output + "{"+ Environment.NewLine;
-                    output = output + "\"id\": null," + Environment.NewLine; //id field
-                    output = output + "\"name\": {" + Environment.NewLine;
-                    output = output + "\"value\": \"" + currentLine + "\"," + Environment.NewLine; //name field
-                    output = output + "\"synonyms\": [" + Environment.NewLine;
-                    output = output + "\"" + currentLine + "\"," + Environment.NewLine; //name field
-                    output = output + "\"" + currentLine + " station\"" + Environment.NewLine; //name field
-                    output = output + "]" + Environment.NewLine;
-                    output = output + "}" + Environment.NewLine;
-                    output = output + "},"+ Environment.NewLine;
+                    string[] pass = currentLine.Split(',');
+                    pass = pass.Where(x => !string.IsNullOrEmpty(x)).ToArray(); //remove empties 
+                    int number = pass.Count();
+                    if (number == 1)
+                    {
+                        output = output + "{" + Environment.NewLine;
+                        output = output + "\"id\": null," + Environment.NewLine; //id field
+                        output = output + "\"name\": {" + Environment.NewLine;
+                        output = output + "\"value\": \"" + currentLine + "\"," + Environment.NewLine; //name field
+                        output = output + "\"synonyms\": [" + Environment.NewLine;
+                        output = output + "\"" + currentLine + "\"," + Environment.NewLine; //name field
+                        output = output + "\"" + currentLine + " station\"" + Environment.NewLine; //name field
+                        output = output + "]" + Environment.NewLine;
+                        output = output + "}" + Environment.NewLine;
+                        output = output + "}," + Environment.NewLine;
+                    }
+                    else
+                    {
+                        output = output + "{" + Environment.NewLine;
+                        output = output + "\"id\": null," + Environment.NewLine; //id field
+                        output = output + "\"name\": {" + Environment.NewLine;
+                        output = output + "\"value\": \"" + pass[0] + "\"," + Environment.NewLine; //name field
+                        output = output + "\"synonyms\": [" + Environment.NewLine;
+                        for (int i = 0; i < number; i++)
+                        {
+                            if (i != number - 1)
+                            { 
+                                output = output + "\"" + pass[i] + "\"," + Environment.NewLine; //name field
+                            }
+                            else
+                            {
+                                output = output + "\"" + pass[i] + "\"" + Environment.NewLine; //name field
+                            }
+                        }
+
+                        output = output + "]" + Environment.NewLine;
+                        output = output + "}" + Environment.NewLine;
+                        output = output + "}," + Environment.NewLine;
+                    }
                 }
             }
             File.WriteAllText(@"C:\Git\Alexa-Custom-Slot-Generator\output.txt", output);
